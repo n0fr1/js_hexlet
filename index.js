c03ba7cd24df1b3f45e9fc75715144597b98e709
Evgeniy <n0fr1@mail.ru>
2023-04-30 13:00:25 +0300

    add lucky numbers

--- index.js
+++ index.js
@@ -37,13 +37,13 @@ function fizzBuss(begin, end){
     }
 }
 
-//переворот заданного числа c учетом минуса
+//переворот заданного числа (c учетом возможного минуса числа)
 function reverseInt(num){
     const strNum = Math.abs(num).toString();
     let i = 0;   
     let result = '';
     for (i = 0; i < strNum.length; i += 1) {
-        result = `${strNum[i]}${result}`;    
+        result = `${strNum[i]}${result}`;
     }
     if (num !== Math.abs(num)) { 
         result = `-${result}`;
@@ -52,7 +52,7 @@ function reverseInt(num){
     return result;     
 }
 
-//проверка числа на счастливое (обязательно число четное и первая половина = второй)
+//проверка числа на счастливое (обязательно требование: число четное)
 function isHappyTicket(ticket){
     let i = 0;
     const half = ticket.length / 2;
@@ -65,5 +65,71 @@ function isHappyTicket(ticket){
             secondPart += Number(ticket[i-1]);    
         }
     }
-    return firstPart === secondPart === true;
+    return firstPart === secondPart;
 }
+
+//проверка на совершенно число (положительное целое число, равное сумме его положительных делителей)
+function isPerfect(num) {
+    if (num < 6) {
+      return false;
+    }
+    let del = 1;
+    let i = 2;
+    const half = num / 2;
+    while (i <= half) {
+      if (num % i === 0) {
+        del += i;
+      }
+      i += 1;
+    }
+    return num === del;
+}
+
+//1. инвертируем строку (заглавная буква => строчная и наоборот) с использованием регулярок.
+function invertCaseRegular(str) {
+    let result = '';
+    for (let j = 0; j < str.length; j += 1) {
+        if (/[a-zа-яё]/i.test(str[j])) {
+            if (str[j] === str[j].toUpperCase()) {
+                result += `${str[j].toLowerCase()}`;
+            }
+            if (str[j] === str[j].toLowerCase()) {
+                result += `${str[j].toUpperCase()}`;                
+            }
+        } else {
+            result += `${str[j]}`;            
+        }    
+    }
+    return result;
+}
+
+function isHappyNumber(num) {
+    if (num === 0) {
+        return false;
+    }
+    if (num === 1) {
+        return true;
+    }
+    let result = Math.pow(num, 2);
+    let curExpResult = 0;
+    //let test = 1;
+    for (let i = 0; i < 10; i += 1) {        
+        console.log(result);
+        if (result === 1) {
+            break;
+        } 
+        result = String(result);
+        for (let j = 0; j < result.length; j += 1) {            
+            let curExp = Math.pow(Number(result[j]), 2);
+            curExpResult += curExp;            
+        }    
+        result = curExpResult;
+        // if (test === 4) {
+        //     console.log(curExpResult);                
+        // } 
+        curExpResult = 0;                
+        //test += 1;
+    }
+    return num === 1;
+} 
+isHappyNumber(7);
